bc3d9e080792b3a6436273c15a91cbf4a73931dd
Trevor Osterman <trevormosterman@gmail.com>
2020-05-31 10:02:39 -0700

    switching to single page layout

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React from "react";
-import {Route} from 'react-router-dom';
-import Services from './Services/Services'
+import { Route } from "react-router-dom";
+import Services from "./Services/Services";
 import "./App.css";
 import Nav from "./Nav/Nav.js";
-import Hero from './Hero/Hero.js'
-import About from './About/About.js'
-import Contact from './Contact/Contact'
+import Hero from "./Hero/Hero.js";
+import About from "./About/About.js";
+import Contact from "./Contact/Contact";
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <Route path exact="/" component={Hero} />
-      <Route path exact="/" component={About} />
-      <Route path="/contact" component={Contact} />
-      <Route path="/services" component={Services}/>
+      <div className="header">
+        <Hero />
+        <Nav />
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
