a1b81537395ddafc42d63730d74f0fef35b928a1
Trevor Osterman <trevormosterman@gmail.com>
2020-05-31 17:13:48 -0700

    improvements to scroll experience

--- src/Services/Services.js
+++ src/Services/Services.js
@@ -3,7 +3,7 @@ import "./Services.css";
 
 const Calendly = () => {
   return (
-    <div id="calendar" style={{ height: "800px" }}>
+    <div id="calendar" style={{ height: "50vh" }}>
       <iframe
         src="https://calendly.com/dresdenosterman"
         width="80%"
