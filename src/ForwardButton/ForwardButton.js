9072cc5b9a14355a38949ee42fc82afd0ad8c776
Trevor Osterman <trevormosterman@gmail.com>
2020-06-30 17:23:21 -0700

    arrow functionality implemented

--- src/ForwardButton/ForwardButton.js
+++ src/ForwardButton/ForwardButton.js
@@ -1,13 +1,23 @@
 import React from "react";
 import "./ForwardButton.css";
 
+const paths = [`#about`, `#services`, `#contact`];
+
 export default function ForwardButton() {
   return (
     <button
       className="forward-button"
       style={{ height: `100px`, width: `100px` }}
       onClick={() => {
-        console.log(window.location.hash);
+        for (let i = 0; i < paths.length; i++) {
+          if (
+            paths[i] === window.location.hash &&
+            paths[i] !== paths[paths.length - 1]
+          ) {
+            console.log(paths[i++]);
+            window.location.hash = paths[i++];
+          }
+        }
       }}
     >
       >
