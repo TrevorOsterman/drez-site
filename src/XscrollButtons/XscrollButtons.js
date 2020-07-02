90547db615137c82f980fa3e0aca46cd2cd1dffe
Trevor Osterman <trevormosterman@gmail.com>
2020-07-01 18:21:11 -0700

    back button created and logic began

--- src/XscrollButtons/XscrollButtons.js
+++ src/XscrollButtons/XscrollButtons.js
@@ -5,26 +5,45 @@ const paths = [`#about`, `#services`, `#contact`];
 
 export default function XscrollButtons() {
   return (
-    <button
-      className="forward-button"
-      style={{ height: `100px`, width: `100px` }}
-      onClick={() => {
-        if (!window.location.hash) {
-          window.location.hash = paths[1];
-        } else {
-          for (let i = 0; i < paths.length; i++) {
-            if (
-              paths[i] === window.location.hash &&
-              paths[i] !== paths[paths.length - 1]
-            ) {
-              console.log(paths[i++]);
-              window.location.hash = paths[i++];
+    <>
+      <button
+        className="forward-button"
+        style={{ height: `100px`, width: `100px` }}
+        onClick={() => {
+          if (!window.location.hash) {
+            window.location.hash = paths[1];
+          } else {
+            for (let i = 0; i < paths.length; i++) {
+              if (
+                paths[i] === window.location.hash &&
+                paths[i] !== paths[paths.length - 1]
+              ) {
+                console.log(paths[i++]);
+                window.location.hash = paths[i++];
+              }
             }
           }
-        }
-      }}
-    >
-      &gt;
-    </button>
+        }}
+      >
+        &gt;
+      </button>
+      <button
+        className="back-button"
+        style={{ height: `100px`, width: `100px` }}
+        onClick={() => {
+          if (!window.location.hash) {
+            window.location.hash = ``;
+          }
+          for (let i = 0; i < paths.length; i++) {
+            if (paths[i] === window.location.hash) {
+              console.log(paths[i++]);
+              window.location.hash = paths[i--];
+            }
+          }
+        }}
+      >
+        fuck
+      </button>
+    </>
   );
 }
