444c55e3bdb1b2c321193fcd50bfb063e542ff23
Trevor Osterman <trevormosterman@gmail.com>
2020-07-03 09:57:15 -0700

    content card formatted

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -4,10 +4,10 @@ import "./Contact.css";
 export default function Contact() {
   return (
     <div id="contact" className="contact-section card">
-      <h1 className="comp-header">Contact</h1>
-      <h2>Dresden Osterman</h2>
-      <span>Email: dresdenosterman@cox.net</span>
-      <span>Cell: (623) 570-1964</span>
+      <h1 className="contact-header">Contact</h1>
+      <h2 className="contact-name">Dresden Osterman</h2>
+      <span className="contact-info">Email: dresdenosterman@cox.net</span>
+      <span className="contact-info">Cell: (623) 570-1964</span>
     </div>
   );
 }
